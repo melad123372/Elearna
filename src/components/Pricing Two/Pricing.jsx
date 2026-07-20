c9468672183274636c95a5f63256380a68d8ce2b
melad123372 <meladraufi707@gmail.com>
2026-07-20 07:27:45 +0430

    Add missing assets for Render deployment

--- src/components/Pricing Two/Pricing.jsx
+++ src/components/Pricing Two/Pricing.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { FiCheckCircle, FiXCircle } from "react-icons/fi";
 
-import gift from "../../assets/Pricing Two/gift.svg";
-import king from "../../assets/Pricing Two/king.svg";
+import gift from "../../assets/Pricing-Two/gift.svg";
+import king from "../../assets/Pricing-Two/king.svg";
 
 const plans = [
   {

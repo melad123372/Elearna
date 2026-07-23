ab04cc7b5b73ca817c51070738223a0d817a77e4
melad123372 <meladraufi707@gmail.com>
2026-07-23 13:40:40 +0430

    Fix import errors

--- src/components/Home one/Homeone.jsx
+++ src/components/Home one/Homeone.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import Home from "../Home one/Home";
-import LogoSection from "../Home one/LogoSection";
-import TopCategories from "../Home one/TopCategories";
-import QuickCourses from "../Home one/QuickCourses";
-import Courses from "../Home one/Courses";
-import PersonalizedLearning from "../Home one/PersonalizedLearning";
-import Testimonials from "../Home one/Testimonials";
+
+import Home from "./Home";
+import LogoSection from "./LogoSection";
+import TopCategories from "./TopCategories";
+import QuickCourses from "./QuickCourses";
+import Courses from "./courses";
+import PersonalizedLearning from "./PersonalizedLearning";
+import Testimonials from "./Testimonials";
 import PopularCourses from "./CourseExplorer";
-import WhyWeAre from "../Home one/WhyWeAre";
+import WhyWeAre from "./WhyWeAre";
 import Footer from "./Footer";
+
 function Homeone() {
   return (
     <div>
